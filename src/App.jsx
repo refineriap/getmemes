e50a87b163c2bc9ab6a1b3b01c96629c72c99636
refineriap <refineriap9@gmail.com>
2024-05-04 21:36:06 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+import image from '../src/assets/react.svg'
 import './App.css'
 
 function App() {
-  
+  return (
+    <div>
+      <img src={image} className="image"/>
+    </div>
+  )
 }
 
 export default App
