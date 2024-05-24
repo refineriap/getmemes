92b383c9dee88c9d7ffd768e30c311c932109592
refineriap <refineriap9@gmail.com>
2024-05-24 17:24:32 -0400

    getImage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,19 @@
+import memesData from "../memesData"
+
+
 
 export default function Main() {
-    function handleClick(event) {
-        event.preventDefault();
-        console.log("I was clicked!!!")
+    function getMeme() {
+        const getImage = memesData.data.memes
+        const randNum = Math.floor(Math.random() * getImage.length)
+        const url = getImage[randNum].url
+        console.log(url)
     }
 
-    function handleOnMouseOver( ) {
-        
-        console.log("I entered the image!!")
-    }
 
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <div>
                     <label htmlFor="top-text">Click for top text</label>            
                     <input id="top-text" type="text" className="form--input" placeholder="top text" />
@@ -21,9 +22,9 @@ export default function Main() {
                     <label htmlFor="bottom--text">Click for bottom text</label>   
                     <input id="bottom--text" type="text" className="form--input" placeholder="bottom text" />
                 </div>           
-                <button onClick={handleClick} className="form--button" >Get a new meme image</button>
-                <img onMouseOver={handleOnMouseOver} src="sk-uVPV_nV17Tw-unsplash.jpg" alt="" className="image--practice" />
-            </form>
+                <button onClick={getMeme} className="form--button" >Get a new meme image</button>
+                <img src="sk-uVPV_nV17Tw-unsplash.jpg" alt="" className="image--practice" />
+            </div>
         </main>
     )
 }
