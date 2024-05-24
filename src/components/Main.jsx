b9b1087311c3dd632d6889439e0e8976384a5736
refineriap <refineriap9@gmail.com>
2024-05-24 15:23:06 -0400

    click events

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,28 @@
 
 export default function Main() {
-    function handleClick() {
+    function handleClick(event) {
+        event.preventDefault();
         console.log("I was clicked!!!")
+    }
+
+    function handleOnMouseOver( ) {
+        
+        console.log("I entered the image!!")
     }
 
     return (
         <main>
             <form className="form">
                 <div>
-                    <label htmlFor="top-text">Click for top search</label>            
+                    <label htmlFor="top-text">Click for top text</label>            
                     <input id="top-text" type="text" className="form--input" placeholder="top text" />
                 </div> 
                 <div>
-                    <label htmlFor="bottom--text">Click for bottom search</label>   
+                    <label htmlFor="bottom--text">Click for bottom text</label>   
                     <input id="bottom--text" type="text" className="form--input" placeholder="bottom text" />
                 </div>           
-                <button className="form--button" onClick={handleClick}>Get a new meme image</button>
+                <button onClick={handleClick} className="form--button" >Get a new meme image</button>
+                <img onMouseOver={handleOnMouseOver} src="sk-uVPV_nV17Tw-unsplash.jpg" alt="" className="image--practice" />
             </form>
         </main>
     )
